28232b90140f7ef0f99c6d8e246b6b3b187a7931
dglgmut <florez.renan@gmail.com>
2014-09-05 00:24:22 -0300

    Calling github api insetead of json test

--- src/main/scala/GitConsumer.scala
+++ src/main/scala/GitConsumer.scala
@@ -1,9 +1,14 @@
 package awe.somo.githubconsumer
-import scalaj.http.Http
+import scalaj.http.{Http, Token, HttpOptions}
 
 object GitConsumer {
   def main(args: Array[String]) {
-    val request: Http.Request = Http("http://date.jsontest.com/")
+    println("Insert your API token")
+    val consumer = readLine()
+    val request: Http.Request = Http("https://api.github.com/user").
+                                  auth(consumer, "").
+                                  option(HttpOptions.connTimeout(1000)).
+                                  option(HttpOptions.readTimeout(5000))
 
     val resultOne = request.asString
     println(resultOne)
