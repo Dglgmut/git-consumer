800d6f3151b2587c9c9552609d717f2f368166eb
dglgmut <florez.renan@gmail.com>
2014-09-05 00:41:09 -0300

    Creating get data method for pullrequests

--- src/main/scala/GitConsumer.scala
+++ src/main/scala/GitConsumer.scala
@@ -2,13 +2,20 @@ package awe.somo.githubconsumer
 import scalaj.http.{Http, Token, HttpOptions}
 
 object GitConsumer {
+  def getData(consumer: String, repository: String) : Http.Request = {
+    Http(s"https://api.github.com/repos/$repository/pulls").
+      auth(consumer, "").
+      param("state","open").
+      option(HttpOptions.connTimeout(1000)).
+      option(HttpOptions.readTimeout(5000))
+  }
+
   def main(args: Array[String]) {
     println("Insert your API token")
     val consumer = readLine()
-    val request: Http.Request = Http("https://api.github.com/user").
-                                  auth(consumer, "").
-                                  option(HttpOptions.connTimeout(1000)).
-                                  option(HttpOptions.readTimeout(5000))
+    println("Insert the repository name")
+    val repository = readLine()
+    val request = getData(consumer, repository)
 
     val resultOne = request.asString
     println(resultOne)
